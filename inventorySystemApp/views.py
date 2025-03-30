95f90b7977678cafdf3e277cf3e3ee1e602da5ad
Frank_Gonzalez <frankgonzalezlopez24@gmail.com>
2025-03-30 15:41:48 -0400

    Login Page Added, Css Added, Static File Added

--- inventorySystemApp/views.py
+++ inventorySystemApp/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password are wrong.')
 
-    return render(request, "inventorySystemApp/login.html")
+    return render(request, 'inventorySystemApp/login.html')
 
 
 def home(request):
